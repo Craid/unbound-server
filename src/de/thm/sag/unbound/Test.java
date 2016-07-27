05c09a4a8a65880477b54a3e3b9801a13606ef2e
Craid <Craid@Games>
2016-07-27 11:00:09 +0200

    Moved in new class

--- src/de/thm/sag/unbound/Test.java
+++
@@ -1,5 +0,0 @@
-package de.thm.sag.unbound;
-
-public class Test {
-
-}
---
+++ src/de/unbound/game/logic/ServerSurvivalGameUpdate.java
@@ -0,0 +1,40 @@
+package de.unbound.game.logic;
+
+import de.unbound.game.logic.AbstractGameUpdate;
+
+
+public class ServerSurvivalGameUpdate extends AbstractGameUpdate{
+
+	@Override
+	public boolean isGameOver() {
+		return battleField.getMainBase().isActive();
+	}	
+	
+	@Override
+	public void doBeforeUpdate() {
+		//get updated entites!
+		//add them to battlefield!
+	}
+
+	@Override
+	public void doAfterUpdate() {
+		//Do nothing normally rendering
+	}
+
+	@Override
+	public void onGameEnd() {
+		//send game Over Message
+	}
+
+	@Override
+	public void onCollisionHandling(double deltaTime) {
+		collisionDetection.update(deltaTime);
+	}
+
+	@Override
+	public void updateWaveHandler(double deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
